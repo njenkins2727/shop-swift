c7dd7843946467c608d36bb9cce3d916ac13750d
njenkins2727 <njenkins2727@gmail.com>
2024-01-24 13:56:20 +0800

    File structure complete. Html layout complete, Css added and will touch-up at the end. javascript functionality next aim

---
+++ app.js
@@ -0,0 +1,23 @@
+//Grab all elements
+const formDiv = document.getElementById('form')
+let userInput = document.getElementById('user-input')
+const formBtn = document.getElementById('form-btn')
+
+const listDiv = document.getElementById('list')
+const listUl = document.getElementById('list-cards');
+
+let listArr = [];
+
+//add event listener for submit form 
+formBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('working button');
+})
+//make object instance class 
+
+//display listItems in the dom 
+
+//Add items into localStorage 
+
+//
+
