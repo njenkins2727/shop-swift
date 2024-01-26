6d5b393879ff2a58cef2c49aaec9717dbd664f8e
njenkins2727 <njenkins2727@gmail.com>
2024-01-26 18:38:14 +0800

    localstorage items get added to list, added css to these items.

--- app.js
+++ app.js
@@ -4,53 +4,57 @@ let userInput = document.getElementById('user-input')
 const formBtn = document.getElementById('form-btn')
 
 const listDiv = document.getElementById('list')
-const listUl = document.getElementById('list-cards');
+const listUl = document.getElementById('list-card');
 
 let listArr = [];
 
-
-//add event listener for submit form 
-formBtn.addEventListener('click', function(event){
-    event.preventDefault();
-    
-//Saves items to localstorage as an array 
-    if(typeof(Storage) !== "undefined") {
-        if (localStorage.getItem('item') && localStorage.getItem('item').length > 0)
-            listArr = JSON.parse(localStorage.getItem('item'));
-            let dataObj = userInput.value;
-            listArr.push(dataObj);
-            localStorage.setItem('item', JSON.stringify(listArr));
-
-        } else {
-        Error('Something went wrong.');
-    }
-})
-//make object instance class 
-
+//Make object instance class 
 class Item {
     constructor(name){
         this.name = name;
     }
-
-//Do you add functionality here?
-//add item to DOM 
-    static addToList(name){
-        // retrivedOjects = localStorage.getItem('item');
-        console.log(name)
+    static displayItems(name){
         //add elements to make card 
         //loop through local sotrage array and append items into card 
+        for (let i = 0; i < name.length; i++) {
+            const element = name[i];
+            const newEl = document.createElement('li');
+            newEl.classList.add('list-items')
+            listUl.appendChild(newEl)
+            newEl.innerHTML = element;
+        }
+        
     }
 
+    static addItem(name){
+        //if item is or is not in array in local storage
+            //append to list 
+            console.log(name[name.length - 1]);
+            const newEl = document.createElement('li');
+            newEl.classList.add('list-items');
+            listUl.appendChild(newEl)
+            newEl.innerHTML = name[name.length - 1];
+    }
 }
-//display listItems in the dom 
+
+//add event listener for submit form 
+formBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    //Saves items to localstorage as an array 
+    if(typeof(Storage) !== "undefined") {
+        if (localStorage.getItem('item') && localStorage.getItem('item').length > 0)
+        listArr = JSON.parse(localStorage.getItem('item'));
+    let dataObj = userInput.value;
+    listArr.push(dataObj);
+    localStorage.setItem('item', JSON.stringify(listArr));
+    
+} else {
+    Error('Something went wrong.');
+}
 retrivedOjects = JSON.parse(localStorage.getItem('item'));
-Item.addToList(retrivedOjects);
+Item.addItem(retrivedOjects);
 
-//Add items into localStorage 
+})
 
-
-
-//WHEN I GET BACK 
-
-// Item isnt being imported from file item.js, Maybe i need to download a pack? node.js or npm ??
-// create static function in item.js class Item 
+retrivedOjects = JSON.parse(localStorage.getItem('item'));
+Item.displayItems(retrivedOjects);
